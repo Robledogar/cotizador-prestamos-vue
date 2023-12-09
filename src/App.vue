4289183c3bb08f2a9dd54aead43b4a66f8e6a208
robledogar <robledogar@gmail.com>
2023-12-09 10:42:17 +0100

    Añadiendo atributos dinámicos

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,64 @@
 <script setup>
+import {ref, computed} from 'vue'
+  import Header from './components/Header.vue'
+
+  const cantidad = ref(10000);
+  const MIN = 0;
+  const MAX = 20000;
+  const STEP = 100;
+
+  const formatearDinero = computed(() => {
+    const formatter = new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD'
+    })
+
+    return formatter.format(cantidad.value)
+  });
+
+  
+
+
 </script>
 
 <template>
- <h1 class="text-4xl font-black">Hola mundo</h1>
+ <div class="my-20 max-w-lg mx-auto bg-white shadow p-10">
+    <Header />
+
+    <div class="my-5">
+      <input 
+      type="range"
+      class="w-full bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      :min="MIN"
+      :max="MAX"
+      :step="STEP"
+      v-model.number="cantidad"
+      >
+
+      <p class="text-center my-10 text-5xl font-extrabold text-indigo-600">{{ formatearDinero }}</p>
+      
+    </div>
+ </div>
   
 </template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 <!-- <style scoped>
 .logo {
